2628f484869844cb819626a27b3acf9eedf28f18
megalowdonny <megalowdonny@gmail.com>
2022-02-17 15:36:05 -0800

    Button input and display update done

--- scripts.js
+++ scripts.js
@@ -5,6 +5,9 @@ const display = document.querySelector('.numberDisplay');
 const buttonSection = document.querySelector('.buttonSection');
 const numberButtons = document.querySelector('.numberButtons');
 const operandButtons = document.querySelector('.operandButtons');
+let numbers = []; // Eventual nodelist
+let operands = []; // Eventual nodelist
+let currentValue = display.textContent;
 
 /* Building the Calculator */
 
@@ -12,11 +15,13 @@ const numberList = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0]
 numberList.forEach(i => { 
   createButton(i, numberButtons);
 });
+numbers = Array.from(numberButtons.querySelectorAll('.button'));
 
 const operandList = ['*', '/', '-', '+', '='];
 operandList.forEach(i => {
   createButton(i, operandButtons);
 })
+operands = Array.from(operandButtons.querySelectorAll('.button'));
 
 /* Functions */
 
@@ -28,3 +33,25 @@ function createButton(i, list) {
   button.dataset.value = i;
   list.appendChild(button);
 }
+
+function updateDisplay() {
+  display.textContent = currentValue;
+}
+
+/* Event handlers */
+
+function handleNumber(e) {
+  const input = e.target.dataset.value;
+  console.log(input);
+  currentValue += input;
+  updateDisplay();
+}
+
+function handleOperand(e) {
+  console.log(e.target.dataset.value);
+}
+
+/* Event listeners */
+
+numbers.forEach(number => number.addEventListener('click', handleNumber));
+operands.forEach(number => number.addEventListener('click', handleOperand));
